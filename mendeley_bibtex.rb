cf2d8e05ae2f1245c84bfc3c9ab2212795241a42
Eriko Nishimoto <eriko@gfd-dennou.org>
2012-09-10 17:51:41 +0900

    doiを項目に入れるかと、euc_jpへの変換、をオプションで選べるように変更

--- mendeley_bibtex.rb
+++ mendeley_bibtex.rb
@@ -3,15 +3,61 @@
 #  mendeleyで生成された *.bib ファイルから
 #  不必要な行を削除するプログラム
 
+require 'getoptlong.rb'
+
+parser = GetoptLong.new
+parser.set_options(
+  ['--input','--i', GetoptLong::REQUIRED_ARGUMENT],
+  ['--output','--o', GetoptLong::REQUIRED_ARGUMENT],
+  ['--nodoi', GetoptLong::NO_ARGUMENT],
+  ['--nonkf', GetoptLong::NO_ARGUMENT],
+  ['--help', '-h', GetoptLong::NO_ARGUMENT]
+)
+
+
+#-- initial values
 input = './library.bib'
 output = './reference.bib'
 #output = './test.bib'
+doi = true
+euc = true
+help = false
+
+#-- get input values
+parser.each_option do |name,arg|
+  name.sub!(/^--/,'')
+  case name
+  when 'input' , 'i'
+    input = arg.to_s
+  when 'output' , 'o'
+    output = arg.to_s
+  when 'nodoi'
+    doi = false
+  when 'nonkf'
+    euc = false
+  when 'help' , 'h'
+    help = true
+  end
+end
+
+#-- help message
+if (help)
+  usage = "Usage: #{$0.split('/')[-1]}
+    --i, --input [string]     specify a output png file (default './library.bib')
+    --o, --output [string]    specify a output png file (default './reference.bib')
+    --nodoi                   exclude doi (default false)
+    --nonkf                   not trun into euc_jp (default false)
+    -h, --help                show this message"
+  puts usage
+  exit
+end
+
+#-- including_list
 
 including_list =
 ["@",\
 "author = {",\
 "editor = {",\
-"doi = {",\
 "issn = {",\
 "journal = {",\
 "month = {",\
@@ -21,7 +67,27 @@ including_list =
 "volume = {",\
 "year = {",\
 "booktitle = {",\
+]
+including_list << "doi = {" if doi
+including_list << "}"
+
+=begin
+including_list =
+["@",\
+"author = {",\
+"editor = {",\
+"issn = {",\
+"journal = {",\
+"month = {",\
+"number = {",\
+"pages = {",\
+"title = {",\
+"volume = {",\
+"year = {",\
+"booktitle = {",\
+"doi = {",\
 "}"]
+=end
 
 ###-- $ grep "^@\|^author" library.bib
 
@@ -37,6 +103,8 @@ puts command
 system( command )
 
 ###-- $ nkf
-nkf = (`nkf --guess #{output}`).split[0]
-system("nkf -euc --overwrite #{output}") if nkf!="EUC-JP"
+if euc
+  nkf = (`nkf --guess #{output}`).split[0]
+  system("nkf -euc --overwrite #{output}") if nkf!="EUC-JP"
+end
 
